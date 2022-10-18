f432795bcf7227b6624cc8bbd9c832c9d7f7fbd7
Heo-mi-gyeong <https://github.com/Heo-mi-gyeong/information.git>
2022-10-18 14:30:55 +0900

    useRef 추가

--- src/component/itemlist/ItemList.js
+++ src/component/itemlist/ItemList.js
@@ -1,20 +1,27 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import Button from '../button/Button';
 import TodoItem from '../todoItem/TodoItem';
 import styles from '../../styles/itemList.module.css'
 
 const ItemList = ({item,setItem}) => {
-    const [inputContent,setInputContent] = useState("");
+    //const [inputContent,setInputContent] = useState("");
+    const inputContent = useRef();
     const cnt = useRef(0);
 
     const onClick = (e) => {
         e.preventDefault();
-        if(!inputContent){
+        if(!inputContent.current.value){
+            alert("일정을 입력해주세요.");
+            return;
+        }
+        setItem([...item, {id : cnt.current++, content : inputContent.current.value}]);
+        inputContent.current.value = '';
+        /* if(!inputContent){
             alert("일정을 입력해주세요.");
             return;
         }
         setItem([...item, {id : cnt.current++, content : inputContent}]);
-        setInputContent('');
+        setInputContent(''); */
     }
 
     const remove = (targetId) =>{
@@ -28,7 +35,7 @@ const ItemList = ({item,setItem}) => {
     const modify = (targetId, targetContent) => {
         const newItem = Object.assign(item);
         newItem.map((data, i) => {
-            if(targetId === i && data.content) {
+            if(targetId === data.id && data.content) {
                 data.content = targetContent;
             }
         });
@@ -39,7 +46,8 @@ const ItemList = ({item,setItem}) => {
     <div>
         <div className={styles.box}>
             <form>
-                <input className={styles.inputBox} type={'text'} value={inputContent} onChange={e => setInputContent(e.target.value)} placeholder='일정을 입력하세요.'/>
+            {/* <input className={styles.inputBox} type={'text'} value={inputContent} onChange={e => setInputContent(e.target.value)} placeholder='일정을 입력하세요.'/> */}
+                <input className={styles.inputBox} type={'text'} placeholder='일정을 입력하세요.' ref={inputContent}/>
                 <Button text={'등록'} onClick={onClick}/>
             </form>
         </div>
